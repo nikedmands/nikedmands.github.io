913369beac063ce9a75d97e35306dcde7d32da60
Nik Edmands <nikedmands@gmail.com>
2023-06-13 15:07:39 +0100

    css scroll images

--- js/css_scroll.js
+++ js/css_scroll.js
@@ -4,8 +4,8 @@ let sections = document.querySelectorAll('section');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        // 'offest' value is stored as top of window-150
-        let offset = sec.offsetTop -150;
+        // 'offest' value is stored as top of window-200
+        let offset = sec.offsetTop -200;
         let height = sec.offsetHeight;
 
         // show animation when the section is in window view
